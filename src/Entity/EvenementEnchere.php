fa11a1ae469ea4b717ab9ef9ec72e49331901d91
karima <karimabelabbas701@gmail.com>
2025-09-15 10:59:05 +0200

    Correction des relations entre les entités et uniformisation des noms de classes

--- src/Entity/EvenementEnchere.php
+++ src/Entity/EvenementEnchere.php
@@ -3,6 +3,9 @@
 namespace App\Entity;
 
 use App\Repository\EvenementEnchereRepository;
+use App\Entity\Lot;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EvenementEnchereRepository::class)]
@@ -16,6 +19,9 @@ class EvenementEnchere
     #[ORM\Column(nullable: true)]
     private ?float $EvenementEnchere = null;
 
+    #[ORM\OneToMany(mappedBy: 'evenementEnchere', targetEntity: Lot::class, orphanRemoval: true)]
+    private Collection $lots;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -24,6 +30,36 @@ class EvenementEnchere
     public function getEvenementEnchere(): ?float
     {
         return $this->EvenementEnchere;
+    }
+
+    /**
+     * @return Collection<int, Lot>
+     */
+    public function getLots(): Collection
+    {
+        return $this->lots;
+    }
+
+    public function addLot(Lot $lot): self
+    {
+        if (!$this->lots->contains($lot)) {
+            $this->lots[] = $lot;
+            $lot->setEvenementEnchere($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLot(Lot $lot): self
+    {
+        if ($this->lots->removeElement($lot)) {
+            // set the owning side to null (unless already changed)
+            if ($lot->getEvenementEnchere() === $this) {
+                $lot->setEvenementEnchere(null);
+            }
+        }
+
+        return $this;
     }
 
     public function setEvenementEnchere(?float $EvenementEnchere): static
