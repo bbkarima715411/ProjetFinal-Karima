264f11ad695958cc31c612cd6ebec92913fb7547
karima <karimabelabbas701@gmail.com>
2025-09-30 14:15:09 +0200

    Modifications des fichiers Entity et Controller

--- src/Entity/EnchereUtilisateur.php
+++ src/Entity/EnchereUtilisateur.php
@@ -8,14 +8,12 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: EnchereUtilisateurRepository::class)]
 class EnchereUtilisateur
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Id] #[ORM\GeneratedValue] #[ORM\Column]
     private ?int $id = null;
 
-    // Montant de l'enchère
-    #[ORM\Column(nullable: true)]
-    private ?float $montant = null;
+    // NON NULL maintenant
+    #[ORM\Column]
+    private float $montant = 0.0;
 
     #[ORM\ManyToOne(inversedBy: 'encheresUtilisateur')]
     #[ORM\JoinColumn(nullable: false)]
@@ -25,14 +23,26 @@ class EnchereUtilisateur
     #[ORM\JoinColumn(nullable: false)]
     private ?Utilisateur $utilisateur = null;
 
+    // Nouveau : date
+    #[ORM\Column(type: 'datetime_immutable')]
+    private \DateTimeImmutable $creeLe;
+
+    public function __construct()
+    {
+        $this->creeLe = new \DateTimeImmutable();
+    }
+
     public function getId(): ?int { return $this->id; }
 
-    public function getMontant(): ?float { return $this->montant; }
-    public function setMontant(?float $montant): self { $this->montant = $montant; return $this; }
+    public function getMontant(): float { return $this->montant; }
+    public function setMontant(float $montant): self { $this->montant = $montant; return $this; }
 
     public function getLot(): ?Lot { return $this->lot; }
     public function setLot(?Lot $lot): self { $this->lot = $lot; return $this; }
 
     public function getUtilisateur(): ?Utilisateur { return $this->utilisateur; }
     public function setUtilisateur(?Utilisateur $utilisateur): self { $this->utilisateur = $utilisateur; return $this; }
+
+    public function getCreeLe(): \DateTimeImmutable { return $this->creeLe; }
+    public function setCreeLe(\DateTimeImmutable $d): self { $this->creeLe = $d; return $this; }
 }
