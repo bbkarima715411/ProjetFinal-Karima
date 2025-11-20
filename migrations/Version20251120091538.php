109fae2642280dbc8f344a3294d879359acc07a4
karima <karimabelabbas701@gmail.com>
2025-11-20 22:39:37 +0100

    changement de certaine page

--- migrations/Version20251120091538.php
+++ migrations/Version20251120091538.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251117225408 extends AbstractMigration
+final class Version20251120091538 extends AbstractMigration
 {
     public function getDescription(): string
     {
