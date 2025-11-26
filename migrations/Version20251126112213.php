c576e829ebaf987d5011263126d4c1a93d5b517a
karima <karimabelabbas701@gmail.com>
2025-11-26 12:39:30 +0100

    Ma version locale que je veux garder

--- migrations/Version20251126112213.php
+++ migrations/Version20251126112213.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251120091538 extends AbstractMigration
+final class Version20251126112213 extends AbstractMigration
 {
     public function getDescription(): string
     {
