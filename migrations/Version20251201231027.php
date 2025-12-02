9661cbbe80139f462ef301e79bf05274b8403d7b
karima <karimabelabbas701@gmail.com>
2025-12-02 01:09:38 +0100

    version

--- migrations/Version20251201231027.php
+++ migrations/Version20251201231027.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251201204203 extends AbstractMigration
+final class Version20251201231027 extends AbstractMigration
 {
     public function getDescription(): string
     {
